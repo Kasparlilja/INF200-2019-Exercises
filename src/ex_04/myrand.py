02542a1573283116f39e81f0edfea3cab2b0e11f
Kaspar Lilja <kalilja@nmbu.no>
2019-10-23 16:35:17 +0200

    Finished ex_04

--- src/ex_04/myrand.py
+++ src/ex_04/myrand.py
@@ -3,12 +3,41 @@
 __author__ = ''
 __email__ = '@nmbu.no'
 
-import random
-
 class LCGRand:
-    def __init__(self):
-        self.a = 16807
-        self.b = 2**31-1
-        r[n + 1] = a * r[n] % m
+    def __init__(self, seed):
+        self.seed = seed
+
 
     def rand(self):
+        a = 16807
+        m = 2 ** 31 - 1
+        self.seed = (a * self.seed) % m
+        return self.seed
+
+
+class ListRand:
+    def __init__(self, randomlist):
+
+        self.randomlist = randomlist
+        self.position = 0
+
+
+    def rand(self):
+
+        if self.position >= len(self.randomlist):
+            raise RuntimeError
+        else:
+            temp_pos = self.position
+            self.position += 1
+            return self.randomlist[temp_pos]
+
+
+if __name__ == "__main__":
+    rng1 = LCGRand(1)
+    rng2 = LCGRand(2)
+    print(rng1.rand(), rng2.rand())
+
+    rng3 = ListRand([1, 4])
+    print(rng3.rand())
+    print(rng3.rand())
+    print(rng3.rand())  # testing if a RuntimeError raises
--- src/ex_04/walker.py
+++ src/ex_04/walker.py
@@ -6,4 +6,51 @@ __email__ = '@nmbu.no'
 import random
 
 class Walker:
-    def
+
+    def __init__(self, initialpos, homepos):
+        self.initalpos = initialpos
+        self.homepos = homepos
+        self.steps = 0
+
+
+    def move(self):
+        self.initalpos += random.choice([-1, 1])
+        self.steps += 1
+
+
+    def is_at_home(self):
+        if self.homepos == self.initalpos:
+            return True
+        else:
+            return False
+
+
+    def get_position(self):
+        return self.initalpos
+
+
+    def get_steps(self):
+        return self.steps
+
+
+def walking(distance):
+
+    startpos = 0
+    the_walker = Walker(startpos, distance)
+
+    while the_walker.is_at_home() == False:
+        the_walker.move()
+
+    return the_walker.get_steps()
+
+
+if __name__ == "__main__":
+
+    distances = [1, 2, 5, 10, 20, 50, 100]
+    for distance in distances:
+        path = []
+        for i in range(5):
+            path.append(walking(distance))
+        print('Distance: {0} -> Path lengths: {1}'.format(distance, path))
+
+
